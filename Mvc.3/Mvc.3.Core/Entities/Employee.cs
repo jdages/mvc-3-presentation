1e65caa5f66c741b3b59e15791c059ece7f93caf
jdages <jdages@woh.rr.com>
2011-02-20 21:58:31 -0500

    Added validation hooks

--- Mvc.3/Mvc.3.Core/Entities/Employee.cs
+++ Mvc.3/Mvc.3.Core/Entities/Employee.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace Mvc3.Core.Entities
 {
-        public class Employee
-        {
-            public string Name { get; set; }
-            public DateTime HireDate { get; set; }
-            public DateTime StartDate { get; set; }
-        }
-    
+    public class Employee 
+    {
+        public string Name { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+
 }
---
+++ Mvc.3/Mvc.3.Web/Scripts/Custom.Validations.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="jquery-1.4.4-vsdoc.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="jquery.unobtrusive-ajax.js" />
+
+$.validator.addMethod
+(
+    'startingLetter',
+    function (inboundValue, element, parameters) {
+        return true;
+    },
+    'Invalid starting letter'
+);
